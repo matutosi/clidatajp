edf198e6665071e396e387455e81ebe1bdb67bdb
matutosi <matutosi@gmail.com>
2022-11-03 15:30:58 +0900

    version 0.5.1

--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -5,7 +5,11 @@ test_that("download_area_links(), download_links() and download_climate() works"
     stringr::str_c("https://www.data.jma.go.jp/gmd/cpd/monitor/nrmlist/", 
       c("CountryList.php?rcode=01", "CountryList.php?rcode=02", "CountryList.php?rcode=03", 
         "CountryList.php?rcode=04", "CountryList.php?rcode=05", "CountryList.php?rcode=06"))
-  expect_equal(area, exep)
+  if(is.null(area)){
+    skip("download_area_links() is not available")
+  }else{
+    expect_equal(area, exep)
+  }
 
     # country links
   country <- download_links(sample(area, 1))
